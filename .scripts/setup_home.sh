acce4c89f790f67a76556b874c2d8dc38ded02d1
Manuel Meraz <meraz.manuel@gmail.com>
2019-04-23 20:10:08 +0000

    moved spinner to setup_home script, cleaned up output

--- .scripts/setup_home.sh
+++ .scripts/setup_home.sh
@@ -1,19 +1,33 @@
 #! /usr/bin/env bash
 
+# Shows a spinner while running  a command
+spinner() {
+  $@ &> /dev/null &
+  PID=$!
+  i=1
+  sp="/-\|"
+  #echo -n ' '
+  while [ -d /proc/$PID ]
+  do
+    printf "\b${sp:i++%${#sp}:1}"
+  done
+  printf "\b"
+}
+
 applications=("vim" "tmux" "silversearcher-ag" "git" "htop" 
               "tree" "openssh-server" "openssh-client" "tansmission"
-              "geany" "bash-completion")
+              "geany" "bash-completion" "cmake")
 
 echo "Installing favorite applications..."
-spinner sudo apt -qq update &> /dev/null
+spinner sudo apt -qq update 
 
 for app in ${applications[@]}; do
   echo "$app"
-  sudo apt -qq install $app -y &> /dev/null
+  spinner sudo apt -qq install $app -y
 done
 
 echo "spotify"
-snap install spotify &> /dev/null
+spinner snap install spotify
 
 echo "Setup git submodules (.vim, projects/ etc)..."
 git submodule update --init --recursive > /dev/null
