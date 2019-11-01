99c4ee865be837d400af048a86629951e53b618d
Manuel Meraz <meraz.manuel@gmail.com>
2019-11-01 12:36:12 +0000

    add ros to setup

--- .scripts/setup_home.sh
+++ .scripts/setup_home.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+ROS_DISTRO="melodic"
+
 if [[ -e $HOME/.home_setup ]]; then
   echo "Home is already setup!"
   return 1
@@ -10,7 +12,9 @@ applications=("vim" "tmux" "silversearcher-ag" "git" "htop"
               "geany" "bash-completion" "cmake" "gcc" "g++"
               "python3-dev" "python-dev" "python3-pip" "python-pip"
               "build-essential" "clang" "clang-tidy" "clang-format" 
-              "clang-tools" "gdb")
+              "clang-tools" "gdb" "ros-$ROS_DISTRO-desktop-full"
+              "python-rosinstall" "python-rosinstall-generator" 
+              "python-wstool" "build-essential" "python-catkin-tools")
 
 
 echo "Sudo is required. Password might be prompted"
@@ -22,14 +26,21 @@ if [[ $? != 0 ]]; then
 fi
 
 echo "Updating and upgrading ubuntu..."
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
 sudo apt -qq update
 sudo apt -qq upgrade -y
+
 
 echo "Installing favorite applications..."
 for app in ${applications[@]}; do
   echo "$app"
   sudo apt -qq install $app -y
 done
+
+sudo rosdep init
+rosdep update
 
 sudo snap install spotify
 sudo snap install universal-ctags
@@ -58,6 +69,9 @@ fi
 
 git pull origin master
 git update-index --assume-unchanged $HOME/.profile
+
+echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
+source ~/.bashrc
 
 echo "Setting up .vim and projects submodules..."
 git submodule update --init --recursive --remote
