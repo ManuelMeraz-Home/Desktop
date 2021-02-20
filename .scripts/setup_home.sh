659b6f244b5143476d3eb2eed9da2354bfb630fb
Manuel Meraz <meraz.manuel@gmail.com>
2021-02-20 16:35:13 -0500

    correct bugs in setup home script

--- .scripts/setup_home.sh
+++ .scripts/setup_home.sh
@@ -5,7 +5,8 @@ if [[ -e $HOME/.home_setup ]]; then
   return 1
 fi
 
-reboot () { echo 'Reboot? Need to restart to complete Installation. (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot; }
+request_reboot () { echo 'Reboot? Need to restart to complete Installation. (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot; }
+request_install_displaylink_drivers () { echo 'Install displaylink drivers?. (y/n)' && read x && [[ "$x" == "y" ]] && sudo $HOME/.scripts/displaylink_driver.sh; }
 
 applications=("vim" "tmux" "silversearcher-ag" "git" "htop" 
               "tree" "openssh-server" "openssh-client" 
@@ -23,49 +24,50 @@ if [[ $? != 0 ]]; then
   return 1
 fi
 
-sudo usermod -aG docker $(whoami) || exit 1
+sudo usermod -aG docker $(whoami) || return 1
 
 echo "Updating, upgrading, and Installing favorite applications..."
-sudo apt -qq update && sudo apt -qq upgrade -y && sudo apt -qq install -y ${applications[*]} || exit 1
+sudo apt -qq update && sudo apt -qq upgrade -y && sudo apt -qq install -y ${applications[*]} || return 1
 
 echo "Setting up git home repository"
 if [ ! -d $HOME/.ssh ]; then
-    mkdir $HOME/.ssh || exit 1
+    mkdir $HOME/.ssh || return 1
 fi
 
 if [ ! -e $HOME/.ssh/config ]; then
-    wget https://gist.githubusercontent.com/ManuelMeraz/d216bdca170766b053a110b97abc6648/raw/d56e5acbb891f0e37f1564bd03d5e12e0a5d4bac/config -P $HOME/.ssh || exit 1
+    wget https://gist.githubusercontent.com/ManuelMeraz/d216bdca170766b053a110b97abc6648/raw/d56e5acbb891f0e37f1564bd03d5e12e0a5d4bac/config -P $HOME/.ssh || return 1
 fi
 
 if [ ! -e $HOME/.ssh/known_hosts ]; then
-    touch $HOME/.ssh/known_hosts || exit 1
+    touch $HOME/.ssh/known_hosts || return 1
 fi
    
 if [ -e $HOME/.profile ]; then
-    rm $HOME/.profile || exit 1
+    rm $HOME/.profile || return 1
 fi
 
 if [ -e $HOME/.bashrc ]; then
-    rm $HOME/.bashrc || exit 1
+    rm $HOME/.bashrc || return 1
 fi
 
 (ssh-keygen -F github.com || ssh-keyscan github.com >> $HOME/.ssh/known_hosts)
-git init && git remote add origin https://github.com/manuelmeraz/home.git || exit 1
+git init && git remote add origin https://github.com/manuelmeraz/home.git || return 1
 
 git pull origin master
-git update-index --assume-unchanged $HOME/.profile || exit 1
+git update-index --assume-unchanged $HOME/.profile || return 1
 
-source ~/.bashrc || exit 1
+source ~/.bashrc || return 1
 
 echo "Setting up .vim and projects submodules..."
-git submodule update --init --recursive --remote || exit 1
-$HOME/.vim/setup.sh || exit 1
+git submodule update --init --recursive --remote || return 1
+$HOME/.vim/setup.sh || return 1
 
 touch $HOME/.home_setup
-source ~/.profile || exit 1
+source ~/.profile || return 1
 
-rm -rf $HOME/.git && rm $HOME/README.md && rm $HOME/LICENSE && $HOME/projects/README.md || exit 1
+rm -rf $HOME/.git && rm $HOME/README.md && rm $HOME/LICENSE && rm $HOME/projects/README.md || return 1
 
+request_install_displaylink_drivers 
 request_reboot 
 echo "Done!"
 
